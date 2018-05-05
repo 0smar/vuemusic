79b35d5eff1369861776c2a39e627942c267bdd1
0smar <osmar.roramas@gmail.com>
2018-05-05 16:52:50 -0500

    added loader and dinamic artist list

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,54 @@
 <template lang="pug">
   #app
     img(src='./assets/logo.png')
-    h1 {{ msg }}
-    h2 Essential Links
+    h1 VueMusic
+    select(v-model="selectedCountry")
+      option(v-for="country in countries" v-bind:value="country.value") {{ country.name }}
+    loader(v-show="loading")
     ul
-      li
-        a(href='https://vuejs.org', target='_blank') Core Docs
-      li
-        a(href='https://forum.vuejs.org', target='_blank') Forum
-      li
-        a(href='https://chat.vuejs.org', target='_blank') Community Chat
-      li
-        a(href='https://twitter.com/vuejs', target='_blank') Twitter
-    h2 Ecosystem
-    ul
-      li
-        a(href='http://router.vuejs.org/', target='_blank') vue-router
-      li
-        a(href='http://vuex.vuejs.org/', target='_blank') vuex
-      li
-        a(href='http://vue-loader.vuejs.org/', target='_blank') vue-loader
-      li
-        a(href='https://github.com/vuejs/awesome-vue', target='_blank') awesome-vue
-
+      artist(v-for="artist in artists" v-bind:artist="artist" v-bind:key="artist.mbid")
 </template>
 
 <script>
+import Artist from './components/Artist.vue'
+import Loader from './components/Loader.vue'
+import getArtists from './api'
 export default {
   name: 'app',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      artists: [],
+      countries: [
+        { name: 'Mexico', value: 'mexico'},
+        { name: 'Argentina', value: 'argentina'},
+        { name: 'España', value: 'spain'}
+      ],
+      selectedCountry: 'mexico',
+      loading: true
+    }
+  },
+  components: {
+    Artist,
+    Loader
+  },
+  methods: {
+    refreshArtists() {
+      const self = this
+      this.loading = true
+      this.artists = []
+      getArtists(this.selectedCountry)
+        .then( function (artists) {
+          self.loading = false
+          self.artists = artists
+        })
+    }
+  },
+  mounted() {
+    this.refreshArtists()
+  },
+  watch: {
+    selectedCountry(){
+      this.refreshArtists()
     }
   }
 }
---
+++ src/api/config.js
@@ -0,0 +1,8 @@
+export default {
+	appname: 'vuemusic',
+	apiKey: '8c6fa78b5f0a8a8fd7e9f45f46db92a2',
+	secret: '4b74bf40f3fe61c4ea07cab2bf9dc6da',
+	registeredTo: 'OsmarRamirez'
+}
+
+
